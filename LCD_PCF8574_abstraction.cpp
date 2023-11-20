21ae4c1ae402493d601c23d8004adfee7d689748
Distaff <distaff.pk@gmail.com>
2023-11-21 00:45:59 +0100

    wip

--- LCD_PCF8574_abstraction.cpp
+++ LCD_PCF8574_abstraction.cpp
@@ -1,9 +1,47 @@
 #include "LCD_PCF8574_abstraction.h"
 
-LCD_PCF8574::LCD_PCF8574(uint8_t colCount, uint8_t rowCount, std::function<int(uint8_t)> sendByte, std::function<void(int)> delayUs) 
+namespace {
+    constexpr uint8_t INIT_CODE1        = 0b00000011;
+    constexpr uint8_t INIT_CODE2        = 0b00000010;
+
+    // Commands
+    constexpr uint8_t NO_COMMAND        = 0;
+    constexpr uint8_t CLEAR             = 0b00000001;
+    constexpr uint8_t HOME              = 0b00000010;
+    constexpr uint8_t ENTRY_MODE_SET    = 0b00000100;
+    constexpr uint8_t DISPLAY_CONTROL   = 0b00001000;
+    constexpr uint8_t CURSOR_SHIFT      = 0b00010000;
+    constexpr uint8_t FUNCTION_SET      = 0b00100000;   // Function set command. Pin config is 0 0 1 DL N F - - with DL, N and F set by flags below
+    constexpr uint8_t SET_CGRAM_ADDR    = 0b01000000;   // Set CGRAM address command. Pin config is 0 1 ACG AC5 AC4 AC3 AC2 AC1 AC0
+    constexpr uint8_t SET_DDRAM_ADDR    = 0b10000000;   // Set DDRAM address command. Pin config is 1 ADD ADD ADD ADD ADD ADD ADD ADD
+
+    // Bit masks for setting flags in commands
+    constexpr uint8_t ENTRY_MODE_SET_ID = 0b00000010;   // Bit high for increment mode, low for decrement mode (left-to-right or right-to-left)
+    constexpr uint8_t ENTRY_MODE_SET_S  = 0b00000001;   // Bit high for shift display, low for shift cursor
+
+    constexpr uint8_t DISPLAY_CONTROL_D = 0b00000100;   // Bit high for display on, low for display off
+    constexpr uint8_t DISPLAY_CONTROL_C = 0b00000010;   // Bit high for cursor on, low for cursor off
+    constexpr uint8_t DISPLAY_CONTROL_B = 0b00000001;   // Bit high for cursor blink on, low for cursor blink off
+
+    constexpr uint8_t FUNCTION_SET_DL   = 0b00010000;   // Bit high for 8-bit mode, low for 4-bit mode
+    constexpr uint8_t FUNCTION_SET_N    = 0b00001000;   // Bit high for 2-line mode, low for 1-line mode
+    constexpr uint8_t FUNCTION_SET_F    = 0b00000100;   // Bit high for 5x10 font, low for 5x8 font
+}
+
+
+/*** public ***/
+
+LCD_PCF8574::LCD_PCF8574(uint8_t colCount, uint8_t rowCount, std::function<size_t(uint8_t)> sendByte, std::function<void(int)> delayUs) 
     : colCount_(colCount), rowCount_(rowCount), sendByte_(sendByte), delayUs_(delayUs)
 {
-
+    state_ = {
+        .displayEnabled     = true,
+        .backlightEnabled   = true,
+        .cursorVisible      = true,
+        .cursorBlink        = true,
+        .incrementMode      = true,
+        .shiftDisplay       = false
+    };
 }
 
 void LCD_PCF8574::begin()
@@ -15,17 +53,55 @@ void LCD_PCF8574::begin()
         rowOffsets_[2] = 0x00 + colCount_;
         rowOffsets_[3] = 0x40 + colCount_;
     }
+
+    delayUs_(50000);
+
+    // Set all PCF8574 pins to LOW
+    sendByte_(0x00);
+    delayUs_(50000);
+
+    /*** Initialization sequence - order specified by HD44780 documentation ***/
+    sendNibble(COMMAND, INIT_CODE1);
+    delayUs_(5000);
+    sendNibble(COMMAND, INIT_CODE1);
+    delayUs_(500);
+    sendNibble(COMMAND, INIT_CODE1);
+    delayUs_(500);
+    sendNibble(COMMAND, INIT_CODE2);
+    delayUs_(500);
+
+    // Set 4-bit mode, 1 or 2-line mode, 5x8 font
+    send(COMMAND, FUNCTION_SET | (rowCount_ > 1 ? FUNCTION_SET_N : 0));
+    delayUs_(250);
+
+    // Set display mode to default values
+    updateDisplayControl();
+    delayUs_(250);
+
+    // Clear display
+    send(COMMAND, CLEAR); 
+    delayUs_(2000);
+
+    // Set entry mode to default values
+    updateEntryMode();
+    delayUs_(250);
 }
 
 void LCD_PCF8574::clear()
 {
-    sendByte_(0x01);
-    delayUs_(1600);
+    send(COMMAND, CLEAR); 
+    delayUs_(2000);
 }
 
+void LCD_PCF8574::home()
+{
+    send(COMMAND, HOME); 
+    delayUs_(37);
+}
 
 bool LCD_PCF8574::setCursor(uint8_t col, uint8_t row)
 {
+    // TODO: make this work
     if (col >= colCount_ || row >= rowCount_)
     {
         return false;
@@ -33,10 +109,9 @@ bool LCD_PCF8574::setCursor(uint8_t col, uint8_t row)
     return sendByte_(0x80 | (rowOffsets_[row] + col));
 }
 
-
 size_t LCD_PCF8574::write(char c)
 {
-	return 0;
+	return send(DATA, c);
 }
 
 size_t LCD_PCF8574::print(const char* str)
@@ -44,7 +119,8 @@ size_t LCD_PCF8574::print(const char* str)
     const char* iter;
     for(iter = str; *iter; iter++)
     {
-        if(!write(*iter))
+        bool success = write(*iter);
+        if(!success)
         {
             break;
         }
@@ -56,24 +132,69 @@ size_t LCD_PCF8574::print(const std::string& str)
 {
     return print(str.c_str());
 }
-/*
-void LCD_PCF8574::setBacklightEnabled(bool enabled);
-{
-    
+
+bool LCD_PCF8574::setBacklightEnabled(bool isTrue){
+    state_.backlightEnabled = isTrue;
+    return sendNibble(COMMAND, NO_COMMAND);
 }
 
-void LCD_PCF8574::setCursorVisible(bool visible);
-{
-    
+bool LCD_PCF8574::setDisplayEnabled(bool isTrue){
+    state_.displayEnabled = isTrue;
+    return updateDisplayControl();
 }
 
-void LCD_PCF8574::shiftLeft();
-{
-    
+bool LCD_PCF8574::setCursorVisible(bool isTrue){
+    state_.cursorVisible = isTrue;
+    return updateDisplayControl();
 }
 
-void LCD_PCF8574::shiftRight();
-{
-    
+bool LCD_PCF8574::setCursorBlink(bool isTrue){
+    state_.cursorBlink = isTrue;
+    return updateDisplayControl();
 }
-*/
+
+
+/*** private ***/
+
+bool LCD_PCF8574::updateEntryMode()
+{
+    uint8_t toSend = ENTRY_MODE_SET 
+        | (state_.incrementMode ? ENTRY_MODE_SET_ID : 0) 
+        | (state_.shiftDisplay ? ENTRY_MODE_SET_S : 0);
+    return send(COMMAND, toSend);
+}
+
+bool LCD_PCF8574::updateDisplayControl()
+{
+    uint8_t toSend = DISPLAY_CONTROL 
+        | (state_.displayEnabled ? DISPLAY_CONTROL_D : 0) 
+        | (state_.cursorVisible ? DISPLAY_CONTROL_C : 0) 
+        | (state_.cursorBlink ? DISPLAY_CONTROL_B : 0);
+    return send(COMMAND, toSend);
+}
+
+bool LCD_PCF8574::send(SendMode mode, uint8_t data)
+{
+    bool success = sendNibble(mode, (data & 0b11110000) >> 4);
+    success = sendNibble(mode, (data & 0b00001111)) && success;
+    return success;
+}
+
+bool LCD_PCF8574::sendNibble(SendMode mode, uint8_t data)
+{
+    uint8_t toSend = 0;
+
+    toSend |= (mode == DATA ? rsPinMask_ : 0);
+    toSend |= (state_.backlightEnabled ? blPinMask_ : 0);
+    
+
+    toSend |= ((data & 0b0001) ? d4PinMask_ : 0);
+    toSend |= ((data & 0b0010) ? d5PinMask_ : 0);
+    toSend |= ((data & 0b0100) ? d6PinMask_ : 0);
+    toSend |= ((data & 0b1000) ? d7PinMask_ : 0);
+
+    sendByte_(toSend | enPinMask_);     //send enable pulse
+    delayUs_(1);                        //enable pulse must be >450ns
+    sendByte_(toSend);
+    return 1;
+}
--- LCD_PCF8574_abstraction.h
+++ LCD_PCF8574_abstraction.h
@@ -1,15 +1,12 @@
 /**
  * \file LCD_PCF8574_abstraction.h
- * \brief Abstraction layer for LCD display with PCF8574 controller. 
+ * \brief Abstraction layer for LCD display based on HD44780 with PCF8574 controller. 
  * \author Piotr Kądziela, https://github.com/Distaff
  * \version 0.1-pre
  * 
  * \section Description
  * May be used with any I2C library.
- * Currently supports only 8-bit mode
 */
-
-
 
 #ifndef PCF8574_ab_h
 #define PCF8574_ab_h
@@ -17,7 +14,6 @@
 #include <string>
 #include <array>
 #include <functional>
-
 
 class LCD_PCF8574
 {
@@ -30,9 +26,11 @@ public:
      * \param delayUs function to delay for specified number of microseconds
      * \warning colCount and rowCount must be equal to the actual number of columns and rows on the display
     */
-    LCD_PCF8574(uint8_t colCount, uint8_t rowCount, std::function<int(uint8_t)> sendByte, std::function<void(int)> delayUs);
+    LCD_PCF8574(uint8_t colCount, uint8_t rowCount, std::function<size_t(uint8_t)> sendByte, std::function<void(int)> delayUs);
 
-
+    /**
+     * Initializes the display. Must be called before any other method.
+    */
 	void begin();
 
     /**
@@ -41,12 +39,17 @@ public:
     void clear();
 
     /**
+     * Sets cursor to 0,0
+    */
+    void home();
+
+    /**
      * Sets cursor to specified position (indexed from 0)
-     * \param col column (0 is default)
-     * \param row row (0 is default)
+     * \param col column
+     * \param row row
      * \return true if success, false otherwise (eg. position out of range)
     */
-    bool setCursor(uint8_t col = 0, uint8_t row = 0);
+    bool setCursor(uint8_t col, uint8_t row);
 
     /**
      * Writes a single character to the display
@@ -72,14 +75,32 @@ public:
     /**
      * Enables/disables backlight
      * \param enabled true to enable, false to disable
+     * \return true if success, false otherwise
     */
-    void setBacklightEnabled(bool enabled = true);
+    bool setBacklightEnabled(bool isTrue = true);
+
+    /**
+     * Enables/disables display
+     * \param enabled true to enable, false to disable
+     * \return true if success, false otherwise
+    */
+    bool setDisplayEnabled(bool isTrue = true);
 
     /**
      * Enables/disables displaying an underscore at the cursor position 
      * \param enabled true to enable, false to disable
+     * \return true if success, false otherwise
     */
-    void setCursorVisible(bool visible = true);
+    bool setCursorVisible(bool isTrue = true);
+
+    /**
+     * Enables/disables blinking of the cursor
+     * \param enabled true to enable, false to disable
+     * \return true if success, false otherwise
+    */
+    bool setCursorBlink(bool isTrue = true);
+
+    /* TODO: entry modes */
 
     /**TODO
      * Shifts display contents and cursor one character left
@@ -97,13 +118,46 @@ public:
     // void defineGlyph(uint8_t index, uint8_t[8] data);
 
 protected:
+    enum SendMode
+    {
+        DATA    = 0,
+        COMMAND = 1
+    };
+    
+    // default pin mapping below
+    uint8_t rsPinMask_ = 0b00000001;
+    uint8_t rwPinMask_ = 0b00000010;
+    uint8_t enPinMask_ = 0b00000100;
+    uint8_t blPinMask_ = 0b00001000;
+    uint8_t d4PinMask_ = 0b00010000;
+    uint8_t d5PinMask_ = 0b00100000;
+    uint8_t d6PinMask_ = 0b01000000;
+    uint8_t d7PinMask_ = 0b10000000;
 
     uint8_t colCount_;  /// number of columns
     uint8_t rowCount_;  /// number of rows
     std::array<uint8_t, 4> rowOffsets_;
-
-    std::function<int(uint8_t)> sendByte_;
+    std::function<size_t(uint8_t)> sendByte_;
     std::function<void(int)> delayUs_;
+
+    struct 
+    {
+        bool displayEnabled     : 1;
+        bool backlightEnabled   : 1;
+        bool cursorVisible      : 1;
+        bool cursorBlink        : 1;
+        bool incrementMode      : 1;
+        bool shiftDisplay       : 1;
+    } state_;
+
+
+
+
+    bool updateEntryMode();
+    bool updateDisplayControl();
+    bool send(SendMode mode, uint8_t data);
+    bool sendNibble(SendMode mode, uint8_t data);
+
 };
 
 #endif
